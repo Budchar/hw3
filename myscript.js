9cb81431a05ecde55fc8d3d41468a07d6284d4c8
noohes <frostbastard@gmail.com>
2018-10-31 23:27:37 +0900

    181031 2327

--- myscript.js
+++ myscript.js
@@ -20,36 +20,39 @@ function myprint() {
     var call_no = document.getElementById("call").value;
     var obj = document.getElementById("career").value;
     var mail = document.getElementById("mail_id").value;
-    var edu = document.getElementById("Edu").getElementsByTagName("li");
-    edu[0].cloneNode(true);
-    document.getElementById("edu_ul").appendChild(edu[0]);
-    var skill = document.getElementById("Skill_table").innerHTML;
-    document.getElementById("out_skill_table").innerHTML = skill;
-    Print_name(name);
-    Print_call_no(call_no);
-    Print_objective(obj);
-    Print_adrs(adrs);
-    Print_mail(mail);
-}
-
-function Print_name(name) {
-    document.getElementById("out_name").innerHTML = name;
-}
-
-function Print_call_no(call_no) {
-    document.getElementById("out_call").innerHTML = call_no;
-}
-
-function Print_objective(obj){
+    document.getElementById("out_name").innerText = name;
+    document.getElementById("out_call").innerText = call_no;
     document.getElementById("out_career").innerText = obj;
-}
+    document.getElementById("out_adrs").innerText = adrs;
+    document.getElementById('out_mail').innerText = mail;
 
-function Print_adrs(adrs) {
-    document.getElementById("out_adrs").innerHTML = adrs;
-}
+    var edu = document.getElementById("Edu");
+    var edu_input = edu.getElementsByTagName('input');
+    for(var i=0; i<edu_input.length; i++){
+        edu.getElementsByTagName('input')[i].readOnly = true;
+    }
+    document.getElementById('out_edu_ul').innerHTML = edu.innerHTML;
 
-function Print_mail(mail){
-    document.getElementById('out_mail').innerHTML = mail;
+    var exp = document.getElementById("exp");
+    var exp_input = exp.getElementsByTagName('input');
+    for(var i=0; i<exp_input.length; i++){
+        exp.getElementsByTagName('input')[i].readOnly = true;
+    }
+    document.getElementById('out_exp_ul').innerHTML = exp.innerHTML;
+
+    var skill = document.getElementById("Skill_table");
+    var skill_input = skill.getElementsByTagName('input');
+    for(var i=0; i<skill_input.length; i++){
+        skill.getElementsByTagName('input')[i].readOnly = true;
+    }
+    document.getElementById("out_skill_table").innerHTML = skill.innerHTML;
+
+    var etc = document.getElementById("etc");
+    var etc_input = etc.getElementsByTagName('input');
+    for(var i=0; i<etc_input.length; i++){
+        etc.getElementsByTagName('input')[i].readOnly = true;
+    }
+    document.getElementById('out_etc_ul').innerHTML = etc.innerHTML;
 }
 
 var loadImg = function(event){
@@ -57,28 +60,33 @@ var loadImg = function(event){
     output.src = URL.createObjectURL(event.target.files[0]);
 }
 
-var Add_li_edu = function(){
-    var li = document.createElement('li');
-    li.innerHTML = '<input type="text">';
-    document.getElementById('Edu').appendChild(li);
-}
-
-var Add_li_exp = function(){
-    var li = document.createElement('li');
-    li.innerHTML = '<input type="text">';
-    document.getElementById('exp').appendChild(li);
-}
-
-var Add_tr = function(){
-    var tr = document.createElement('tr');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    td1.innerHTML = '<input type="text">'
-    td2.innerHTML = '<input type="text">'
-    td3.innerHTML = '<input type="text">'
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    document.getElementById('Skill_table_body').appendChild(tr);
+var Add = {
+    Add_li_edu : function() {
+        var li = document.createElement('li');
+        li.innerHTML = '<input type="text">';
+        document.getElementById('Edu').appendChild(li);
+    },
+    Add_li_exp : function(){
+        var li = document.createElement('li');
+        li.innerHTML = '<input type="text">';
+        document.getElementById('exp').appendChild(li);
+    },
+    Add_li_etc : function(){
+        var li = document.createElement('li');
+        li.innerHTML = '<input type="text">';
+        document.getElementById('etc').appendChild(li);
+    },
+    Add_tr : function(){
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        td1.innerHTML = '<input type="text">'
+        td2.innerHTML = '<input type="text">'
+        td3.innerHTML = '<input type="text">'
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        document.getElementById('Skill_table_body').appendChild(tr);
+    },
 }
